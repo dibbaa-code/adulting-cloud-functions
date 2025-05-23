36e9aab3b6352d82f74009d13d0cc7f9e102f074
Divi157 <divyasaini6798@gmail.com>
2025-05-23 01:10:04 -0700

    trigger cloud function from firestore document creation, updation and deletion events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,66 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Firebase Cloud Functions with Firestore document triggers
+ * See full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+// Import the necessary trigger types from Firebase Functions v2
+import {
+  onDocumentCreated,
+  onDocumentUpdated,
+  onDocumentDeleted,
+} from "firebase-functions/v2/firestore";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// ===== Firestore Triggers =====
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/**
+ * Firestore onCreate trigger - runs when a document is created
+ * This example triggers when a new document is created in the 'users'
+ * collection
+ */
+export const onNewUser = onDocumentCreated("users/{userId}", (event) => {
+  const userId = event.params.userId;
+  const userData = event.data?.data();
+
+  logger.info(`New user created with ID: ${userId}`, userData);
+
+  // You could perform actions like:
+  // - Create default data in other collections
+  // - Send welcome emails
+  // - Update counters
+  return null;
+});
+
+/**
+ * Firestore onUpdate trigger - runs when a document is updated
+ */
+export const onUserUpdated = onDocumentUpdated(
+  "users/{userId}",
+  (event) => {
+    const userId = event.params.userId;
+    const beforeData = event.data?.before.data();
+    const afterData = event.data?.after.data();
+
+    logger.info(
+      `User ${userId} updated`,
+      {before: beforeData, after: afterData}
+    );
+    return null;
+  }
+);
+
+/**
+ * Firestore onDelete trigger - runs when a document is deleted
+ */
+export const onUserDeleted = onDocumentDeleted(
+  "users/{userId}",
+  (event) => {
+    const userId = event.params.userId;
+
+    logger.info(`User ${userId} was deleted`);
+    // Clean up related data
+    return null;
+  }
+);
+
+
